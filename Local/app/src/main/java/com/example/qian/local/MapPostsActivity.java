6d6ba44469c9c8e0623ccd734307d73d5468dba6
Courtney Yoo <c.yoo@wustl.edu>
2017-02-22 11:33:00 -0600

    browse screen in progress

---
+++ Local/app/src/main/java/com/example/qian/local/MapPostsActivity.java
@@ -0,0 +1,20 @@
+package com.example.qian.local;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by cyoo0706 on 2/22/17.
+ */
+
+public class MapPostsActivity extends AppCompatActivity {
+//    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.newPost);
+//    fab.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            Intent newPost = new Intent(BrowseActivity.this, PostActivity.class);
+//            startActivity(newPost);
+//        }
+//    });
+}
