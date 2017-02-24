37ad75de2846c557f9eac8d64ca28edf3f2d4218
Courtney Yoo <c.yoo@wustl.edu>
2017-02-24 09:24:51 -0600

    MapActivity layout created

---
+++ Care2Join/app/src/main/java/com/example/android/care2join/CreatePostActivity.java
@@ -0,0 +1,11 @@
+package com.example.android.care2join;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cyoo0706 on 2/24/17.
+ */
+
+public class CreatePostActivity extends AppCompatActivity {
+
+}
---
+++ Care2Join/app/src/main/java/com/example/android/care2join/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.care2join;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cyoo0706 on 2/22/17.
+ */
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+    }
+}
