7214176cdc2f5048f33f125867905342d60002c8
Qian Wu <jolesia.wu@gmail.com>
2017-03-01 00:31:08 -0600

    user login
    firebase auth

---
+++ CareToJoin/app/src/main/java/com/example/labuser/caretojoin/PostDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.labuser.caretojoin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cyoo0706 on 2/22/17.
+ */
+
+
+public class PostDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_detail);
+    }
+
+}
