142619895ba2de52222c9d3cc0c71fd368c30f79
Labuser <qwu1204>
2017-02-28 23:55:27 -0600

    firebase auth

---
+++ CareToJoin/app/src/main/java/com/example/labuser/caretojoin/CreatePostActivity.java
@@ -0,0 +1,16 @@
+package com.example.labuser.caretojoin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Labuser on 2/26/17.
+ */
+
+public class CreatePostActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_detail);
+    }
+}
