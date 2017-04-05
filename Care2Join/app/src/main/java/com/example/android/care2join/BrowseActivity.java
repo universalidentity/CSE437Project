e9812bba360f4996fdc7699f1e432a7bd4e05ca5
Courtney Yoo <c.yoo@wustl.edu>
2017-04-05 18:01:00 -0500

    Added FusedLocationServces API

--- Care2Join/app/src/main/java/com/example/android/care2join/BrowseActivity.java
+++ Care2Join/app/src/main/java/com/example/android/care2join/BrowseActivity.java
@@ -1,18 +1,35 @@
 package com.example.android.care2join;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -21,14 +38,37 @@ import java.util.ArrayList;
 
 import static android.os.Build.VERSION_CODES.N;
 
-public class BrowseActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class BrowseActivity extends AppCompatActivity
+        implements OnMapReadyCallback,
+            GoogleApiClient.OnConnectionFailedListener,
+            GoogleApiClient.ConnectionCallbacks{
 
+    private static final String TAG = BrowseActivity.class.getSimpleName();
     private GoogleMap mMap;
+    private CameraPosition mCameraPosition;
+    private GoogleApiClient mGoogleApiClient;
+
+    private static final LatLng mDefaultLocation = new LatLng(38.6488, -90.3108);
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+
+    private Location mLastKnownLocation;
+
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browse_activity);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.newPost);
 
@@ -64,20 +104,48 @@ public class BrowseActivity extends AppCompatActivity implements OnMapReadyCallb
     }
 
     @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Play services connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "Play services connection failed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        GoogleMapOptions options = new GoogleMapOptions();
-        options.mapType(GoogleMap.MAP_TYPE_NORMAL);
-        options.zoomControlsEnabled(true);
-        options.zoomGesturesEnabled(true);
-        MapFragment.newInstance(options);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        LatLng wustl = new LatLng(38.6488, -90.3108);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wustl, 50));
-        mMap.addMarker(new MarkerOptions()
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+        if (mLocationPermissionGranted) {
+            Log.d(TAG, "Permission Granted");
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+            mMap.addMarker(new MarkerOptions()
             .title("Wustl")
-            .position(wustl));
+            .position(mDefaultLocation));
+        }
+
+
     }
 
 }
