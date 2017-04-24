1b56d85c8640afe0c8e855c79c678557cadf6a6c
Courtney Yoo <c.yoo@wustl.edu>
2017-04-24 14:15:09 -0500

    Added onClick Intent to submitting on Create Post

--- Care2Join/app/src/main/java/com/example/android/care2join/CreatePostActivity.java
+++ Care2Join/app/src/main/java/com/example/android/care2join/CreatePostActivity.java
@@ -18,21 +18,13 @@ import com.google.firebase.database.FirebaseDatabase;
 
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -48,7 +40,7 @@ import com.google.android.gms.location.LocationRequest;
 public class CreatePostActivity extends AppCompatActivity implements com.google.android.gms.location.LocationListener,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
     private Button mcreate_post_submit;
-    private Button mDropPin;
+    private Button mSaveCurrentLocation;
     private EditText mclass;
     private EditText mduration;
     private FirebaseAuth mAuth;
@@ -89,7 +81,7 @@ public class CreatePostActivity extends AppCompatActivity implements com.google.
             mLastKnownLocation.setLongitude(extras.getDouble("Longitude"));
             mLastKnownLocation.setLatitude(extras.getDouble("Latitude"));
         }
-        mDropPin = (Button) findViewById(R.id.drop_pin);
+        mSaveCurrentLocation = (Button) findViewById(R.id.save_location);
 
         mcreate_post_submit = (Button) findViewById(R.id.create_post_submit);
         mclass = (EditText) findViewById(R.id.create_post_class);
@@ -154,7 +146,7 @@ public class CreatePostActivity extends AppCompatActivity implements com.google.
 
         mAuth = FirebaseAuth.getInstance();
 
-        mDropPin.setOnClickListener(new View.OnClickListener() {
+        mSaveCurrentLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(CreatePostActivity.this, "" + mLastKnownLocation.getLatitude() +
@@ -200,6 +192,8 @@ public class CreatePostActivity extends AppCompatActivity implements com.google.
                     }
                 }
 
+                Intent intent = new Intent(CreatePostActivity.this, Tab_activity.class);
+                startActivity(intent);
             }
         });
 
